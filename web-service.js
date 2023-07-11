957e59002491f8411eba1a2559ef85db650714af
David Watson <davidlwatsonjr@users.noreply.github.com>
2023-07-11 13:31:21 -0400

    Update web service output

--- web-service.js
+++ web-service.js
@@ -7,16 +7,20 @@ const app = express();
 app.get('/', async (req, res) => {
   const { textToNumber } = req.query;
 
-  const jobs = await myPreferredJobResults();
+  const jobs = (await myPreferredJobResults()).map(job => ({
+    full_link: `https://app.usebraintrust.com/jobs/${job.id}`,
+    ...job
+  }));
 
   if (jobs.length > 0 && textToNumber) {
-    const randomJobId = jobs[Math.floor(Math.random() * jobs.length)].id;
+    const randomJobFullLink = jobs[Math.floor(Math.random() * jobs.length)].id;
     await sendText(
-      `${jobs.length} new Braintrust jobs available. https://app.usebraintrust.com/jobs/${randomJobId}/`, textToNumber
+      `${jobs.length} new Braintrust jobs available. ${randomJobFullLink}`, textToNumber
     );
   }
 
-  res.send({ count: jobs.length, jobs });
+
+  res.send({ count: jobs.length, links: jobs.map(job => job.full_link), jobs });
 });
 
 const PORT = process.env.PORT || 8080;
