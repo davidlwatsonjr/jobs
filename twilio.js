9cdf7f9f639424453198df22284c1165d1c5dae5
David Watson <davidlwatsonjr@users.noreply.github.com>
2023-07-11 12:19:27 -0400

    Move textToNumber to web service query param

--- twilio.js
+++ twilio.js
@@ -5,16 +5,15 @@ const {
   TWILIO_ACCOUNT_SID,
   TWILIO_AUTH_TOKEN,
   TWILIO_FROM_NUMBER,
-  TWILIO_TO_NUMBER,
 } = process.env;
 
 const twilioClient = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
-const sendText = async (message) => {
+const sendText = async (message, toNumber) => {
   return await twilioClient.messages.create({
     body: message,
     from: TWILIO_FROM_NUMBER,
-    to: TWILIO_TO_NUMBER,
+    to: toNumber,
   });
 };
 
--- web-service.js
+++ web-service.js
@@ -5,12 +5,14 @@ const { sendText } = require('./twilio');
 const app = express();
 
 app.get('/', async (req, res) => {
+  const { textToNumber } = req.query;
+
   const jobs = await myPreferredJobResults();
 
-  if (jobs.length > 0) {
+  if (jobs.length > 0 && textToNumber) {
     const randomJobId = jobs[Math.floor(Math.random() * jobs.length)].id;
     await sendText(
-      `${jobs.length} new Braintrust jobs available. https://app.usebraintrust.com/jobs/${randomJobId}/`
+      `${jobs.length} new Braintrust jobs available. https://app.usebraintrust.com/jobs/${randomJobId}/`, textToNumber
     );
   }
 
