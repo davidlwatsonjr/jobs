ae41877f58eb3e0af64e9335bfa4e2667cfa6e1d
David Watson <davidlwatsonjr@users.noreply.github.com>
2023-07-14 13:57:46 -0400

    Sort Feed results newest-first

--- feeds.js
+++ feeds.js
@@ -3,12 +3,13 @@ const { parse } = require('rss-to-json');
 const getFeedsResults = async (feedUrls) => {
     const feedResults = await Promise.all(feedUrls.map(feedUrl => parse(feedUrl)));
     const feedItems = feedResults.flatMap(feedResult => feedResult.items);
+    feedItems.sort((a, b) => { return b.created - a.created });
   
     const jobs = feedItems.map(item => ({ title: item.title, link: item.link }));
     const links = jobs.map(job => job.link);
     const count = jobs.length;
   
-    return { count, links, jobs, feedResults };
+    return { count, links, jobs, feedItems, feedResults };
   };
   
   module.exports = { getFeedsResults };
