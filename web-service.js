3345999e2fdb8d12cec3515ccd0da191f07c6d45
David Watson <davidlwatsonjr@users.noreply.github.com>
2023-09-12 12:55:46 -0400

    Genericize feeds view

--- web-service.js
+++ web-service.js
@@ -31,7 +31,7 @@ app.get('/braintrust', async (req, res) => {
 });
 
 app.get('/braintrust.html', async (req, res) => {
-  res.render('braintrust.ejs', { jobs: await myPreferredJobResults(), title: "Braintrust" });
+  res.render('feeds.ejs', { jobs: await myPreferredJobResults(), title: "Braintrust" });
 });
 
 
