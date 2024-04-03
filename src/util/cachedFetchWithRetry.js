efab997dc27de371b4c842a15fecba2e558b70bc
David Watson <davidlwatsonjr@users.noreply.github.com>
2024-04-03 12:45:56 -0400

    Refactor cachedFetchWithRetry

--- src/util/cachedFetchWithRetry.js
+++ src/util/cachedFetchWithRetry.js
@@ -1,42 +1,73 @@
 const memoryCache = {};
 
+const DEFAULT_RETRY_OPTIONS = {
+  retries: 5,
+  retryDelay: (attempt) => Math.pow(2, attempt) * 500,
+};
+
+const DEFAULT_CACHE_LENGTH = 60000;
+
+let fetchFn = global.fetch;
+try {
+  fetchFn = require("fetch-retry")(fetchFn);
+} catch (err) {}
+
+const getCacheKey = (optionsObject) => {
+  return require("crypto")
+    .createHash("md5")
+    .update(JSON.stringify(optionsObject))
+    .digest("hex");
+};
+
+const getCachedResponse = (cacheKey) => {
+  return memoryCache[cacheKey];
+};
+
+const setCachedResponse = (cacheKey, response) => {
+  memoryCache[cacheKey] = response;
+};
+
+const deleteCachedResponse = (cacheKey) => {
+  delete memoryCache[cacheKey];
+};
+
+const fetchAndCache = async (
+  url,
+  options,
+  cacheKey,
+  cacheTTL = DEFAULT_CACHE_LENGTH,
+) => {
+  const fetchResponse = await fetchFn(url, {
+    ...DEFAULT_RETRY_OPTIONS,
+    ...options,
+  });
+  const responseText = await fetchResponse.text();
+  setCachedResponse(cacheKey, responseText);
+  setTimeout(() => {
+    deleteCachedResponse(cacheKey);
+  }, cacheTTL);
+
+  return responseText;
+};
+
 const convertTextToPartialFetchReponse = (text) => ({
   ok: async () => Promise.resolve(true),
   text: async () => Promise.resolve(text),
   json: async () => Promise.resolve(JSON.parse(text)),
 });
 
-const fetch = async (url, options) => {
-  const cacheKeySeed = JSON.stringify({ url, options });
-  const cacheKey = require("crypto")
-    .createHash("md5")
-    .update(cacheKeySeed)
-    .digest("hex");
+const fetch = async (url, options = {}) => {
+  const { cacheTTL, ...fetchOptions } = options;
 
-  if (!memoryCache[cacheKey]) {
-    let fetchFn;
-    try {
-      fetchFn = require("fetch-retry")(global.fetch);
-    } catch (err) {
-      fetchFn = global.fetch;
-    }
+  const cacheKey = getCacheKey({ url, fetchOptions });
 
-    const retryConfig = {
-      retries: 5,
-      retryDelay: (attempt) => Math.pow(2, attempt) * 500,
-    };
+  const responseText =
+    getCachedResponse(cacheKey) ||
+    (await fetchAndCache(url, fetchOptions, cacheKey, cacheTTL));
 
-    const fetchResponse = await fetchFn(url, { ...retryConfig, ...options });
-    memoryCache[cacheKey] = await fetchResponse.text();
+  const response = convertTextToPartialFetchReponse(responseText);
 
-    setTimeout(() => {
-      delete memoryCache[cacheKey];
-    }, 60000);
-  }
-
-  return Promise.resolve(
-    convertTextToPartialFetchReponse(memoryCache[cacheKey]),
-  );
+  return Promise.resolve(response);
 };
 
 module.exports = { fetch };
