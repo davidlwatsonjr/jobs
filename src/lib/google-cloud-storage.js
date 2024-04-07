eceea5131e36ec920bd427a46eac9759fb56b6d2
David Watson <davidlwatsonjr@users.noreply.github.com>
2024-04-07 15:47:41 -0400

    Allow app to work even without GCS

--- src/lib/google-cloud-storage.js
+++ src/lib/google-cloud-storage.js
@@ -2,25 +2,31 @@ const { Storage } = require("@google-cloud/storage");
 
 const { GCS_BUCKET } = process.env;
 
-const storage = new Storage();
-const bucket = storage.bucket(GCS_BUCKET);
+let bucket;
+if (GCS_BUCKET) {
+  bucket = new Storage().bucket(GCS_BUCKET);
+} else {
+  console.warn(
+    "GCS_BUCKET environment variable needs to be set for Google Cloud Storage to work.",
+  );
+}
 
 const getFile = async (name) => {
   return (
     await bucket
-      .file(name)
+      ?.file(name)
       .download()
       .catch(() => null)
   )?.toString();
 };
 
 const saveFile = async (name, data, saveOptions) => {
-  return await bucket.file(name).save(data, saveOptions);
+  return await bucket?.file(name).save(data, saveOptions);
 };
 
 const deleteFile = async (name) => {
   return await bucket
-    .file(name)
+    ?.file(name)
     .delete()
     .catch(() => null);
 };
